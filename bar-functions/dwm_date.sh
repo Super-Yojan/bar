520c45c98348a763c888932d29d06b83fdda0c44
Yojan <gautamyojan0@gmail.com>
2022-06-04 12:23:52 -0400

    config

---
+++ bar-functions/dwm_date.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# A dwm_bar function that shows the current date and time
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+# Date is formatted like like this: "[Mon 01-01-00 00:00:00]"
+
+dwm_date(){
+
+clock=$(date '+%I')
+
+case "$clock" in
+	"00") icon="🕛" ;;
+	"01") icon="🕐" ;;
+	"02") icon="🕑" ;;
+	"03") icon="🕒" ;;
+	"04") icon="🕓" ;;
+	"05") icon="🕔" ;;
+	"06") icon="🕕" ;;
+	"07") icon="🕖" ;;
+	"08") icon="🕗" ;;
+	"09") icon="🕘" ;;
+	"10") icon="🕙" ;;
+	"11") icon="🕚" ;;
+	"12") icon="🕛" ;;
+esac
+
+date "+%Y %b %d (%a) $icon %I:%M%p"
+}
+
+dwm_date
---
+++ bar-functions/dwm_resources.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# A dwm_bar function to display information regarding system memory, CPU temperature, and storage
+# Joe Standring <git@joestandring.com>
+# GNU GPLv3
+
+
+dwm_resources(){
+free --mebi | sed -n '2{p;q}' | awk '{printf ("🧠%2.2fGiB/%2.2fGiB\n", ( $3 / 1024), ($2 / 1024))}'
+df_check_location='/home'
+}
+
+dwm_resources
+
